ae8db390ae03f16154add43fcbba175a6ac22d0d
Emanuel Borges <83500558+emanueleborges@users.noreply.github.com>
2022-01-04 17:24:00 -0400

    api_rest_full_node_mysql_jwt_token_backend

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# api_rest_node_mysql_crud
+# api_rest_node_mysql_crud_jwt_token_backend 
 
-Projeto Api_Rest Node Js Mysql CRUD 
+Projeto Api_Rest Node Js Mysql CRUD JWT Token Backend 
+
+git clone https://github.com/emanueleborges/api_rest_node_mysql_crud
+
+npm install
+
+npm start 
--- mysql.session.sql
+++ mysql.session.sql
@@ -1,1 +1,102 @@
-describe PRODUTOS;
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 04-Jan-2022 às 22:21
+-- Versão do servidor: 10.1.39-MariaDB
+-- versão do PHP: 7.3.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mydb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pedidos`
+--
+
+CREATE TABLE `pedidos` (
+  `idpedidos` int(11) NOT NULL,
+  `idproduto` int(11) NOT NULL,
+  `quantidade` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produtos`
+--
+
+CREATE TABLE `produtos` (
+  `idproduto` int(11) NOT NULL,
+  `nome` varchar(45) NOT NULL,
+  `preco` float NOT NULL,
+  `userid` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `idusuarios` int(11) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `SENHA` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+--
+-- Indexes for table `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD PRIMARY KEY (`idpedidos`),
+  ADD KEY `fk_pedidos_produtos_idx` (`idproduto`);
+
+--
+-- Indexes for table `produtos`
+--
+ALTER TABLE `produtos`
+  ADD PRIMARY KEY (`idproduto`);
+
+--
+-- Indexes for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`idusuarios`);
+
+--
+-- AUTO_INCREMENT for table `pedidos`
+--
+ALTER TABLE `pedidos`
+  MODIFY `idpedidos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `produtos`
+--
+ALTER TABLE `produtos`
+  MODIFY `idproduto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `idusuarios` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
