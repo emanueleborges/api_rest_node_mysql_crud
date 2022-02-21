bc2f2d1df53d865f180f059f1872e7b300f6a67f
Emanuel Borges <emanuel_borges@calcomp-icct.org.br>
2022-02-21 09:21:20 -0400

    api_rest_ful_node_mysql_jwt_token

--- mysql.js
+++ mysql.js
@@ -14,6 +14,10 @@ conn.getConnection((err,connection)=> {
   if(err){
     console.log('Banco de Dados Sem Conexão. Erro: ',err.code);
   } else {
+    connection.query(`CREATE DATABASE IF NOT EXISTS mydb;`);
+    connection.query(`CREATE TABLE IF NOT EXISTS mydb.usuarios (idusuarios INT NOT NULL AUTO_INCREMENT, email VARCHAR(45) NOT NULL, senha VARCHAR(45) NOT NULL, PRIMARY KEY (idusuarios))`);
+    connection.query(`CREATE TABLE IF NOT EXISTS mydb.produtos (idproduto  INT NOT NULL AUTO_INCREMENT, nome VARCHAR(45) NOT NULL, preco FLOAT NOT NULL, PRIMARY KEY (idproduto))`);
+    connection.query(`CREATE TABLE IF NOT EXISTS mydb.pedidos  (idpedidos  INT NOT NULL AUTO_INCREMENT, idproduto INT NOT NULL, quantidade FLOAT NOT NULL, PRIMARY KEY (idpedidos), INDEX fk_pedidos_produtos_idx (idproduto ASC) VISIBLE, CONSTRAINT fk_pedidos_produtos FOREIGN KEY (idproduto) REFERENCES mydb.produtos (idproduto) ON DELETE NO ACTION ON UPDATE NO ACTION)`);
     console.log(`Banco de Dados [`, conn.config.connectionConfig.database, `] Conectado.`);
   }
 });
--- mysql.session.sql
+++ mysql.session.sql
@@ -1,1 +1,34 @@
-select * from usuarios;
+
+CREATE DATABASE mydb IF NOT EXISTS
+
+CREATE TABLE IF NOT EXISTS `mydb`.`produtos` (
+  `idproduto` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45) NOT NULL,
+  `preco` FLOAT NOT NULL,
+  PRIMARY KEY (`idproduto`))
+ENGINE = InnoDB
+
+
+CREATE TABLE IF NOT EXISTS `mydb`.`pedidos` (
+  `idpedidos` INT NOT NULL AUTO_INCREMENT,
+  `idproduto` INT NOT NULL,
+  `quantidade` FLOAT NOT NULL,
+  PRIMARY KEY (`idpedidos`),
+  INDEX `fk_pedidos_produtos_idx` (`idproduto` ASC) VISIBLE,
+  CONSTRAINT `fk_pedidos_produtos`
+    FOREIGN KEY (`idproduto`)
+    REFERENCES `mydb`.`produtos` (`idproduto`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+
+
+CREATE TABLE IF NOT EXISTS `mydb`.`usuarios` (
+  `idusuarios` INT NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(45) NOT NULL,
+  `senha` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idusuarios`))
+ENGINE = InnoDB
+
+
+select * from mydb.usuarios;
